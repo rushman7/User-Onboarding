6395cdbb5530bb5a7cb86650af26a9ed88b01c65
BuildTools <arthurpisakhov1@gmail.com>
2019-09-17 20:56:10 -0400

    step 3

--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from 'axios';
 
-function UserForm({ errors, touched, values }) {
+function UserForm({ errors, touched, values, isSubmitting }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = () => {
+      axios
+        .get('https://reqres.in/api/users')
+        .then(response => {
+          setUsers(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getUsers()
+  }, [])
+
+
   return (
     <Form>
       <div>
@@ -19,7 +38,7 @@ function UserForm({ errors, touched, values }) {
       </div>
       <Field type="checkbox" name="tos" checked={values.tos} />
       Accept TOS <br />
-      <button>Submit!</button>
+      <button type="submit" disabled={isSubmitting}>Submit!</button>
     </Form>
   )
 }
@@ -35,12 +54,24 @@ const FormikUserForm = withFormik({
   },
 
   validationSchema: Yup.object().shape({
+    name: Yup.string().required("Name is required"),
     email: Yup.string().email("Email not valid").required("Email is required"),
-    password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required")
+    password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+    tos: Yup.boolean().isValid(true, "Please accept the Terms of Service")
   }),
 
-  handleSubmit(values) {
-    console.log(values);
+  handleSubmit(values, { setSubmitting, resetForm }) {
+    axios
+      .post('https://reqres.in/api/users', values)
+      .then(res => {
+        console.log(res)
+        resetForm();
+        setSubmitting(false);
+      })
+      .catch(err => {
+        console.log(err)
+        setSubmitting(false);
+      });
   }
 })(UserForm);
 
